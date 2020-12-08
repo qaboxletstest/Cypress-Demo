61889b61fb0fb54515d4e8859ebf4efed34ebdee
Vinod Gulia <30407194+Vinod-Gulia@users.noreply.github.com>
2020-12-08 14:22:25 +0530

    Shadow DOM Page

---
+++ cypress/integration/QABOX/TC_05_ShadowDOM.spec.js
@@ -0,0 +1,73 @@
+describe('Shadow DOM', () => {
+
+    beforeEach(() => {
+        cy.visit('http://127.0.0.1:5500/Help%20Folder/shadowDOM.html')
+    })
+
+    it('Assert H2 text within Light DOM', () => {
+        cy.get('h2')
+            .should('have.text', 'I belong to Regular/Light Dom')
+    });
+
+    it('Type to Textbox within Light DOM', () => {
+        cy.get('input#channelname')
+            .type('QA BOX LET\'S TEST')
+            .should('have.value', 'QA BOX LET\'S TEST')
+    });
+
+    it.skip('Assert H2 text within Shadow DOM', () => {
+        cy.get('button').click()
+        
+    });
+
+    it.skip('Type to Textbox within Shadow DOM', () => {
+        cy.get('button').click()
+        // cy.get('input#name')
+        //     .type('QA BOX LET\'S TEST')
+        //     .should('have.value', 'QA BOX LET\'S TEST')
+        
+    });
+
+    it.skip('External Website', () => {
+        cy.visit('https://books-pwakit.appspot.com/')
+        cy.get('book-app')
+            .shadow()
+            .find('app-toolbar input#input')
+            .type('Software Testing')
+    });
+
+    it.skip('Using jQuery', () => {
+        cy.get('button').click()
+        cy.get('div#shadowHost').should(e => {
+            const [dom] = e.get()
+            expect(dom.shadowRoot.querySelector('h1').textContent).to.eql('I belong to Shadow DOM');
+            // dom.shadowRoot.querySelector('input#name').value = 'QA BOX LET\'S TEST'
+            dom.shadowRoot.getElementById('name').value = 'QA BOX LET\'S TEST'
+        })
+    });
+
+    it.skip('Config includeShadowDom	equal to True', () => {
+        cy.get('button').click()
+        cy.get('h2')
+            .contains('I belong to Shadow DOM')
+            .should('be.visible')
+        cy.get('input#name').type('QA BOX LET\'S TEST')
+            .should('have.value', 'QA BOX LET\'S TEST')
+    });
+
+    it.skip('Config includeShadowDom equal to True as option', () => {
+        cy.get('button').click()
+        cy.get('h2', { includeShadowDom: true })
+            .contains('I belong to Shadow DOM')
+            .should('be.visible')
+        cy.get('input#name', { includeShadowDom: true }).type('QA BOX LET\'S TEST')
+            .should('have.value', 'QA BOX LET\'S TEST')
+    });
+
+    it.skip('External Website - Config includeShadowDom	equal to True', () => {
+        cy.visit('https://books-pwakit.appspot.com/')
+        cy.get('book-app')
+            .find('app-toolbar input#input')
+            .type('Software Testing')
+    });
+});
