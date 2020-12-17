3089b4010188d8df980adf9ceda6748d11d78dd2
Vinod Gulia <30407194+Vinod-Gulia@users.noreply.github.com>
2020-12-17 13:19:54 +0530

    Enabled Xpath

--- cypress/integration/QABOX/TC_07_PopUp.spec.js
+++ cypress/integration/QABOX/TC_07_PopUp.spec.js
@@ -3,33 +3,47 @@ describe('Pop Up Test Suite', () => {
         cy.visit('http://127.0.0.1:5500/Help%20Folder/differentalerttypes.html')
     });
 
-    it.only('Alert - First Way', () => {
-        
+    it('Alert - First Way', () => {
+        cy.on('window:alert', alertText => {
+            expect(alertText).to.eql('I\'m an Alert Box');
+        })
         cy.get('button#alert').click()
     });
 
     it('Alert - Second Way - Multiple Alerts', () => {
-    
-        cy.get('button#miltiplealert').click()
+        const fn = cy.stub()
+        cy.on('window:alert', fn)
+        cy.get('button#miltiplealert').click().then(() => {
+            expect(fn.getCall(0)).to.be.calledWithExactly('First Alert Box')
+            expect(fn.getCall(1)).to.be.calledWithExactly('Second Alert Box')
+            expect(fn.getCall(2)).to.be.calledWithExactly('Third Alert Box')
+        })
             
     });
 
     it('Confirm - First Way', () => {
         cy.on('window:confirm', confirmTxt => {
             expect(confirmTxt).to.eql('I\'m a Confirm Box');
-            // return true
+            // return false
         })
         cy.get('button#confirm').click()
     });
 
     it('Confirm - Second Way', () => {
-        
-        cy.get('button#confirm').click()
+        const stub = cy.stub()
+        stub.onFirstCall().returns(true)
+        cy.on('window:confirm', stub)
+        cy.get('button#confirm').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWithExactly('I\'m a Confirm Box')
+        })
     });
 
     it('Prompt', () => {
-        
-        cy.get('button#prompt').click()
+        cy.window().then(win => {
+            const stub = cy.stub(win, 'prompt')
+            stub.returns('Avi')
+            cy.get('button#prompt').click()
+        }) 
     });
 
     it('Custom Pop Up Dialog from hidden fields', () => {
@@ -41,14 +55,21 @@ describe('Pop Up Test Suite', () => {
 
     it('Custom Pop Up Dialog from function', () => {
         cy.get('button#moddiag').click()
-        // cy.contains('Ok').click()
-        cy.contains('Close').click()
+        cy.contains('Ok').click()
+        // cy.contains('Close').click()
     });
 
-    it('Window pop up', () => {
+    it.only('Window pop up', () => {
         const pop_url = "https://www.youtube.com/c/qaboxletstest/"
-        
+        cy.window().then(win => {
+            const stub = cy.stub(win, 'open').as('windowopen')
+        })
         cy.get('button#winpop').click()
-        
+        cy.get('@windowopen').should('be.calledWith', pop_url)
+        cy.window().then(win => {
+            win.location.href = pop_url
+            cy.get('input#search').type('Cypress by qa box lets test{enter}')
+        })
+
     });
 });
---
+++ cypress/integration/QABOX/TC_08_XPath.spec.js
@@ -0,0 +1,26 @@
+describe("XPath", () => {
+  before(() => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/cyxpath.html");
+  });
+
+  it("GET & WITHIN", () => {
+    cy.visit("http://127.0.0.1:5500/cyxpath.html");
+    cy.get("fieldset#grouptwo input[name='Channel Name']").type(
+      "QA BOX LETS TEST"
+    );
+
+    cy.get("fieldset#groupone").within(() => {
+      cy.get("input[name='Channel Name']").type("LEARN CYPRESS");
+    });
+  });
+  it.only("XPATH & WITHIN", () => {
+    // cy.xpath('//fieldset[@id="grouptwo"]//input[@name="Channel Name"]')
+    //     .type('QA BOX LETS TEST')
+    cy.xpath('//fieldset[@id="grouptwo"]').within(() => {
+      cy.xpath("//input[@name='Channel Name']").type("QA BOX LETS TEST");
+    });
+    // cy.xpath('//fieldset[@id="grouptwo"]')
+    //     .xpath("//input[@name='Channel Name']")
+    //     .type('QA BOX LETS TEST')
+  });
+});
