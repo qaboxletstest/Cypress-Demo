7baad7d616aef0a36fae813dd825e04d5cdb7105
Vinod Gulia <30407194+Vinod-Gulia@users.noreply.github.com>
2020-12-22 15:16:25 +0530

    part 12 done

--- cypress/integration/QABOX/TC_09_FileUpload.spec.js
+++ cypress/integration/QABOX/TC_09_FileUpload.spec.js
@@ -1,34 +1,50 @@
-describe('File Upload Suite', () => {
-    it('Single File Upload - Light Dom', () => {
-        cy.visit('http://127.0.0.1:5500/Help%20Folder/fileupload.html')
-        cy.get('input#file-upload1').attachFile('dog.jpg')
-        cy.get('span#fileName1').should('have.text', "dog.jpg")
-    });
+describe("File Upload Suite", () => {
+  it.skip("Single File Upload - Light Dom", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/fileupload.html");
+    cy.get("input#file-upload1").attachFile("dog.jpg");
+    cy.get("span#fileName1").should("have.text", "dog.jpg");
+  });
 
-    it('Single File Upload - Shadow Dom', () => {
-        cy.visit('http://127.0.0.1:5500/Help%20Folder/fileupload.html')
-        cy.get('button').click()
-        cy.get('input#file-upload2', { includeShadowDom: true }).attachFile('dog.jpg', { force: true })
-        cy.get('span#fileName2', { includeShadowDom: true }).should('have.text', "dog.jpg")
+  it.skip("Single File Upload - Light Dom - Different Name", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/fileupload.html");
+    cy.get("input#file-upload1").attachFile({
+      filePath: "dog.jpg",
+      fileName: "puppy.jpg",
     });
+    cy.get("span#fileName1").should("have.text", "puppy.jpg");
+  });
 
-    it('File Upload - Drag Drop', () => {
-        cy.visit('https://css-tricks.com/examples/DragAndDropFileUploading/')
-        cy.get('#file').attachFile('yey.jpg')
-        // cy.get('#file').attachFile('yey.jpg', { subjectType: 'drag-n-drop' })
-    });
+  it.skip("Single File Upload - Shadow Dom", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/fileupload.html");
+    cy.get("button").click();
+    cy.get("input#file-upload2", {
+      includeShadowDom: true,
+    }).attachFile("dog.jpg");
+    cy.get("span#fileName2", { includeShadowDom: true }).should(
+      "have.text",
+      "dog.jpg"
+    );
+  });
 
-    it.only('Multiple File Upload - Drag Drop', () => {
-        cy.visit('http://127.0.0.1:5500/Help%20Folder/fileupload.html')
-        cy.get('input#file-upload2')
-            .attachFile('dog.jpg')
-            .attachFile('example.json')
-            .attachFile('yey.jpg')
-    });
+  it.skip("File Upload - Drag Drop", () => {
+    cy.visit("https://css-tricks.com/examples/DragAndDropFileUploading/");
+    // cy.get("#file").attachFile("yey.jpg");
+    cy.get("#file").attachFile("yey.jpg", { subjectType: "drag-n-drop" });
+    // cy.get("div.box__success").should("contain.text", "Done!");
+    cy.contains("Done!").should("be.visible");
+  });
 
-    it('Image File Upload - Drag Drop', () => {
-        cy.visit('http://127.0.0.1:5500/Help%20Folder/fileupload.html')
-        // cy.get('div#holder').attachFile('yey.jpg')
-        cy.get('div#holder').attachFile('yey.jpg', { subjectType: 'drag-n-drop' })
-    });
+  it.skip("Multiple File Upload - Drag Drop", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/fileupload.html");
+    cy.get("input#file-upload2")
+      .attachFile("dog.jpg")
+      .attachFile("example.json")
+      .attachFile("yey.jpg");
+  });
+
+  it.oskipnly("Image File Upload - Drag Drop", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/fileupload.html");
+    // cy.get("div#holder").attachFile("yey.jpg");
+    cy.get("div#holder").attachFile("yey.jpg", { subjectType: "drag-n-drop" });
+  });
 });
