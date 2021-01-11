eb2a13e4abcabc8186ca336f5a14567222fd6ced
Vinod Gulia <30407194+Vinod-Gulia@users.noreply.github.com>
2021-01-11 13:43:15 +0530

    Mouse Events Part 16

---
+++ cypress/integration/QABOX/TC_12_MouseEvents.spec.js
@@ -0,0 +1,81 @@
+describe("Mouse Events & Focus", () => {
+  it.skip("Click", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/mouseevents.html");
+    cy.on("window:alert", (txt) => {
+      expect(txt).to.eql("click event is fired");
+    });
+    cy.get("#click").click();
+    // cy.get("#click").trigger("click");
+  });
+
+  it.skip("Double Click", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/mouseevents.html");
+    cy.on("window:alert", (txt) => {
+      expect(txt).to.eql("dblclick event is fired");
+    });
+    // cy.get('#dblclick').dblclick()
+    // cy.get('#dblclick').trigger('dblclick')
+  });
+
+  it.skip("Right Click", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/mouseevents.html");
+    cy.on("window:alert", (txt) => {
+      expect(txt).to.eql("contextmenu event is fired");
+    });
+    // cy.get('#rightclick').rightclick()
+    // cy.get('#rightclick').trigger('contextmenu')
+  });
+
+  it.skip("Right Click Menu - jqueryUI Plugin - Options coming from Data", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/mouseevents.html");
+    cy.on("window:alert", (txt) => {
+      expect(txt).to.eql("contextmenu event is fired");
+    });
+    cy.get("#box2").rightclick();
+    // cy.get('#box2').trigger('contextmenu')
+    cy.contains("Two Oranges").should("be.visible");
+  });
+
+  it.skip("Mouseover and Mouseout", () => {
+    cy.visit("http://127.0.0.1:5500/Help%20Folder/mouseevents.html");
+    cy.on("window:alert", (txt) => {
+      expect(txt).to.eql("show is clicked");
+    });
+    // cy.get('#msover').trigger('mouseover')
+    // cy.get('#show').should('be.visible')
+    // cy.get('#msover').trigger('mouseout')
+    // cy.get('#show').should('not.be.visible')
+
+    // cy.get('#show').click()
+    // cy.get('#show').click({ force: true })
+    // cy.get('#show').invoke('show').click()
+  });
+
+  it.skip("Spice Jet - mousemove Event", () => {
+    cy.visit("https://www.spicejet.com/");
+    cy.get("a#ctl00_HyperLinkLogin").trigger("mouseover");
+    cy.get('a:contains("SpiceClub Members")').trigger("mouseover");
+    cy.get('li>a:contains("Sign up")').click();
+  });
+
+  it.skip("Trends kitchens - Mousemove Event and Allow the state to change", () => {
+    cy.visit("https://trendskitchens.co.nz/");
+    // cy.get(".main-menu.cl-effect-11 > span:nth-of-type(1)").trigger(
+    //   "mousemove"
+    // );
+    // cy.get("span.has-dropdown:nth-of-type(1)").should("be.visible");
+    cy.contains("Our Process").click();
+    cy.url().should("include", "our-process");
+  });
+
+  it.skip("Automation Practice - Event to be triggered is Focus", () => {
+    cy.visit("http://automationpractice.com/index.php");
+    cy.get(".sf-menu.clearfix.menu-content > li:nth-of-type(1)").trigger(
+      "mousemove"
+    );
+    // cy.get(".submenu-container.clearfix.first-in-line-xs").invoke("show");
+    // cy.get('.sf-menu.clearfix.menu-content > li:nth-of-type(1)')
+    //     .trigger('focus').should('have.class', 'sfHover')
+    cy.get('a[title="Tops"]').click({ force: true });
+  });
+});
